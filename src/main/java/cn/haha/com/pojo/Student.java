4075e188d14634c0be5bd16290119a48191b648e
fly-7023 <1625516185@qq.com>
2019-11-17 11:24:12 +0800

    fly 提交 11.17 11：24

--- src/main/java/cn/haha/com/pojo/Student.java
+++ src/main/java/cn/haha/com/pojo/Student.java
@@ -3,5 +3,8 @@ package cn.haha.com.pojo;
 public class Student {
     public static void main(String[] args) {
         System.out.println("hello  xxxxxxxxxx");
+        System.out.println("11111111111111111");
+        System.out.println("22222222222222222");
+        System.out.println("333333333333333333");
     }
 }
