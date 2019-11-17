077f4b74aa6952150394f9cd6f082c30aec7fe5e
fly-7023 <1625516185@qq.com>
2019-11-17 10:57:48 +0800

    fly 提交 11.17 10：57

--- src/main/java/cn/haha/com/pojo/Student.java
+++ src/main/java/cn/haha/com/pojo/Student.java
@@ -1,30 +1,7 @@
 package cn.haha.com.pojo;
 
 public class Student {
-    private String name;
-    private String id;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                '}';
+    public static void main(String[] args) {
+        System.out.println("hello  xxxxxxxxxx");
     }
 }
